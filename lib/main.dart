1c09db274b034b18efedbd2d9395134856e68bfe
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-02-15 15:12:31 +0300

    create a person class

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-  
-  
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
     );
   }
 }
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String emoji;
+
+  const Person({
+    required this.name,
+    required this.age,
+    required this.emoji,
+  });
+}
+
+const List<Person> people = [
+  Person(name: 'John', age: 25, emoji: '👨'),
+  Person(name: 'Jane', age: 30, emoji: '👩'),
+  Person(name: 'Bob', age: 35, emoji: '👨'),
+];
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('People'),
+      ),
+      body: const Center(
+        child: Text('Flutter Animation Challenge 4'),
+      ),
+    );
   }
 }
